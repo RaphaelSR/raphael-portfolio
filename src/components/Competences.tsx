1911ee7feb16c9d6426d8531f3ced8811a099fce
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2023-03-25 21:54:13 -0300

    feat: Add more icons and animation in competences

--- src/components/Competences.tsx
+++ src/components/Competences.tsx
@@ -1,12 +1,22 @@
-// components/Competences.tsx
-import React from "react";
+import React, { useState } from "react";
 import {
   SiNodedotjs,
   SiReact,
   SiTypescript,
   SiHtml5,
   SiCss3,
+  SiFirebase,
+  SiTailwindcss,
+  SiExpo,
+  SiRedux,
+  SiMongodb,
+  SiMysql,
+  SiExpress,
+  SiStyledcomponents,
+  SiSass,
 } from "react-icons/si";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
+import { useTransition, animated } from "react-spring";
 
 interface CompetencesProps {
   competences: {
@@ -20,6 +30,17 @@ interface CompetencesProps {
 }
 
 export function Competences({ competences, titles }: CompetencesProps) {
+  const [showAll, setShowAll] = useState(false);
+  const visibleSkills = showAll
+    ? competences.programming
+    : competences.programming.slice(0, 6);
+
+  const transitions = useTransition(visibleSkills, {
+    keys: (item) => item,
+    from: { opacity: 0, transform: "translate3d(-50px,0,0)" },
+    enter: { opacity: 1, transform: "translate3d(0,0,0)" },
+    leave: { opacity: 0, transform: "translate3d(-50px,0,0)" },
+  });
   function getSkillIcon(skill: string) {
     switch (skill) {
       case "Node.js":
@@ -34,6 +55,24 @@ export function Competences({ competences, titles }: CompetencesProps) {
         return <SiHtml5 className="text-cyan-600" />;
       case "CSS":
         return <SiCss3 className="text-cyan-600" />;
+      case "Firebase":
+        return <SiFirebase className="text-cyan-600" />;
+      case "Tailwind CSS":
+        return <SiTailwindcss className="text-cyan-600" />;
+      case "Expo":
+        return <SiExpo className="text-cyan-600" />;
+      case "Redux":
+        return <SiRedux className="text-cyan-600" />;
+      case "MongoDB":
+        return <SiMongodb className="text-cyan-600" />;
+      case "MySQL":
+        return <SiMysql className="text-cyan-600" />;
+      case "Express":
+        return <SiExpress className="text-cyan-600" />;
+      case "Styled Components":
+        return <SiStyledcomponents className="text-cyan-600" />;
+      case "SASS":
+        return <SiSass className="text-cyan-600" />;
       default:
         return null;
     }
@@ -43,13 +82,27 @@ export function Competences({ competences, titles }: CompetencesProps) {
     <div className="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
       <p>{titles.skillsTitle}</p>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {competences.programming.map((skill, index) => (
-          <div key={index} className="flex items-center space-x-2">
+        {transitions((style, skill, index) => (
+          <animated.div
+            key={index}
+            style={style}
+            className="flex items-center space-x-2"
+          >
             {getSkillIcon(skill)}
             <span className="text-cyan-600">{skill}</span>
-          </div>
+          </animated.div>
         ))}
       </div>
+      <button
+        className="mt-4 text-cyan-600 hover:text-cyan-800 transition-colors duration-200 focus:outline-none"
+        onClick={() => setShowAll(!showAll)}
+      >
+        {showAll ? (
+          <AiOutlineMinus className="text-2xl" />
+        ) : (
+          <AiOutlinePlus className="text-2xl" />
+        )}
+      </button>
     </div>
   );
 }
